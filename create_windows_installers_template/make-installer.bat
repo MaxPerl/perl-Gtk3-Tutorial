8159990096f6e1759823a6c42c757333ce9d205b
MaxPerl <max.augsburg@gmx.de>
2017-07-09 21:26:53 +0200

    Delete make-installer.bat

--- create_windows_installers_template/make-installer.bat
+++
@@ -1,9 +0,0 @@
-C:/msys64/usr/bin/bash.exe -c "./make-installer stage1"
-if errorlevel 1 (
-exit /b %errorlevel%
-)
-
-C:/msys64/usr/bin/bash.exe -c "./make-installer stage2"
-if errorlevel 1 (
-exit /b %errorlevel%
-)
